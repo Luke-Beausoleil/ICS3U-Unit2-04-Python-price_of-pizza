fa9c9a5f2a94c18936822a5833487b02e5bef387
Luke Beausoleil <luke.beausoleil@mths.ca>
2021-04-27 20:02:34 +0000

    added program to find price of pizza and added constants module

---
+++ constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Created by: Luke Beausoleil
+# Created on: April 2021
+# This module contains pizza cost constants
+
+
+LABOR = 0.75
+RENT = 1.00
+MATERIALS = 0.50
+HST = 1.13
---
+++ price_of_pizza.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by: Luke Beausoleil
+# Created on: April 2021
+# This program calculates the price of pizza
+
+import constants
+
+
+def main():
+    # this function calculates the cost of the pizza
+
+    # input
+    diameter = int(input("Enter the diameter of the pizza in inches: "))
+
+    # process
+    subtotal = (
+        (constants.MATERIALS * diameter) + constants.LABOR + constants.RENT
+    )
+    price = subtotal * constants.HST
+
+    # output
+    print("\nThe price of the pizza is: ${:,.2f}".format(price))
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
